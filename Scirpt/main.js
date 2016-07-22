fc84b92a22d5939a72c298c2b391f8e4b4e1a8c5
Jack Taryan <Jack.Taryan@gmail.com>
2016-07-22 15:02:39 +0400

    Slider files

---
+++ Scirpt/main.js
@@ -0,0 +1,14 @@
+var SliderOptions = {
+	SliderId: "Slider",
+	startSlide: 0,
+	effect: "series1",
+	effectRandom: false,
+	pauseTime: 2600,
+	transitionTime: 500,
+	slices: 12,
+	boxes: 8,
+	hoverPause: 1,
+	autoAdvance: true,
+	thumbnailsWrapperId: "thumbs",
+	m: false,
+};
